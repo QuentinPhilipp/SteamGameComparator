ceb278b75eaae22695ff46f22194274e91be3534
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-06-04 17:31:28 +0200

    Get friend info

--- src/app/friend.service.ts
+++ src/app/friend.service.ts
@@ -27,6 +27,36 @@ export class FriendService {
           .subscribe(data =>
             {
         console.log("data :",data);
+
+        let playerListString = "";
+
+        if (data.friendslist.friends.length <100){
+          data.friendslist.friends.forEach(element => {
+            playerListString+=element.steamid+",";
+
+          });
+        }
+        // Request up to 100 user in the same time : https://developer.valvesoftware.com/wiki/Steam_Web_API#GetFriendList_.28v0001.29
+        // GetPlayerSummaries
+
+        // this.http.get("steamapi/ISteamUser/GetPlayerSummaries/v0002/?key=AC5A292DA7F7814A9C54001E2C94D1AF&steamids=76561197960435530")
+        //       .subscribe(dataPlayer =>
+        //         {
+        //           console.log("dataPlayer:",dataPlayer);
+        //         });
+
+        //remove last comma
+        playerListString = playerListString.substring(0, playerListString.length - 1);
+
+        console.log(playerListString);
+
+        this.http.get("steamapi/ISteamUser/GetPlayerSummaries/v0002/?key=AC5A292DA7F7814A9C54001E2C94D1AF&steamids="+playerListString)
+              .subscribe(dataPlayer =>
+                {
+                  console.log("dataPlayer:",dataPlayer);
+                });
+
+        // http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=XXXXXXXXXXXXXXXXXXXXXXX&steamids=76561197960435530
       });
     }
 
